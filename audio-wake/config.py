f8e1237e0221a51546109a702f6bfb00a1feb3a1
Johann Thygs <jthygs@uni-muenster.de>
2025-12-18 00:04:23 +0100

    fixing audio log

--- audio-wake/config.py
+++ audio-wake/config.py
@@ -1,13 +1,3 @@
-# Sheep Configuration
-
-# Audio Settings
-MIC_DEVICE = "plughw:CARD=HD3000,DEV=0"
-SAMPLE_RATE = 44100
-BLOCK_SIZE = 44100 # 1 Second per analysis block
-NOISE_THRESHOLD = 0.05 # Initial guess, calibrate via test script
-
-# Energy Settings (in seconds)
-MAX_ENERGY = 60      # Maximum awake time capacity
 ENERGY_PER_NOISE = 10 # Seconds added per trigger
 IDLE_DRAIN = 1       # Seconds lost per second of silence
 
