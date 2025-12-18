e746bb9d1c204245ff7ac171a14e8f57f9e76ee8
Johann Thygs <jthygs@uni-muenster.de>
2025-12-18 16:17:10 +0100

    Inspect KLAP Revisions

---
+++ admin-display/inspect_klap.py
@@ -0,0 +1,5 @@
+from plugp100.protocol.klap.klap_handshake_revision import KlapHandshakeRevision
+
+print("Available KlapHandshakeRevision members:")
+for member in KlapHandshakeRevision:
+    print(f" - {member.name}")
