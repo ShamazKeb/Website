bd67e387f8871e39462f47eed77eb0ffe005cc30
Johann Thygs <jthygs@uni-muenster.de>
2026-02-01 23:20:29 +0100

    hoffentlich gefixed

--- UPE-website/backend/database.py
+++ UPE-website/backend/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./upe.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/upe.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
